ca8d678528e0f5bc7083aac0076bb43a0cb5e344
LucasAlfare <god-lucas@hotmail.com>
2024-06-19 00:26:12 -0300

    switch from `implementation` to `api` declarations

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,24 +15,24 @@ repositories {
 
 dependencies {
   // Ktor (base and engine)
-  implementation("io.ktor:ktor-server-core:$ktor_version")
-  implementation("io.ktor:ktor-server-netty:$ktor_version")
+  api("io.ktor:ktor-server-core:$ktor_version")
+  api("io.ktor:ktor-server-netty:$ktor_version")
 
   // Serialization
-  implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
-  implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+  api("io.ktor:ktor-server-content-negotiation:$ktor_version")
+  api("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
 
   // CORS...
-  implementation("io.ktor:ktor-server-cors:$ktor_version")
+  api("io.ktor:ktor-server-cors:$ktor_version")
 
   // StatusPages plugin
-  implementation("io.ktor:ktor-server-status-pages:$ktor_version")
+  api("io.ktor:ktor-server-status-pages:$ktor_version")
 
   // Cryptography
-  implementation("org.mindrot:jbcrypt:0.4")
+  api("org.mindrot:jbcrypt:0.4")
 
   // SQL Framework Exposed
-  implementation("org.jetbrains.exposed:exposed-core:0.48.0")
+  api("org.jetbrains.exposed:exposed-core:0.48.0")
 
   /*
   // SQLite dependencies
@@ -42,13 +42,13 @@ dependencies {
     .defaultIsolationLevel = Connection
       .TRANSACTION_SERIALIZABLE
    */
-  implementation("org.xerial:sqlite-jdbc:3.45.2.0")
+  api("org.xerial:sqlite-jdbc:3.45.2.0")
 
   // Postgres dependency
-  implementation("org.postgresql:postgresql:42.7.3")
+  api("org.postgresql:postgresql:42.7.3")
 
   // HikariCP (ConnectionPool)
-  implementation("com.zaxxer:HikariCP:5.1.0")
+  api("com.zaxxer:HikariCP:5.1.0")
 
   testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
