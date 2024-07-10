f6ddd2398bad5b3b5a8f09f9ec6da062ce85c090
LucasAlfare <god-lucas@hotmail.com>
2024-07-10 02:16:48 -0300

    apply serialization plugin in project

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val exposed_version: String by project
 
 plugins {
   kotlin("jvm") version "2.0.0"
+  id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
 }
 
 group = "com.lucasalfare.flbase"
